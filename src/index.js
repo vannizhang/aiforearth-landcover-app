fe9f6cf119b7346f49459e0e247c0a58e6f1c3d0
vannizhang <vannizhang@gmail.com>
2017-12-04 15:14:54 -0800

    added HtmlWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import $ from 'jquery';
+
 console.log('foobar');
+
+$('#rootElement').text('hello world!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: __dirname + '/src/index.js',
     output: {
         path: __dirname + '/build',
         filename: 'bundle.js'
     },
-    module: {
-        loaders: [{ 
-            test: /\.js$/, 
-            exclude: /node_modules/, 
-            loader: "babel-loader" 
-        }]
-    },
     devServer: {
         contentBase: "./build", //本地服务器所加载的页面所在的目录
         inline: true //实时刷新
-    } 
+    },
+    module: {
+        loaders: [
+            { 
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: "babel-loader" 
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, 
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            }
+        
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + "/src/index.template.html"
+        })
+    ],
 };
