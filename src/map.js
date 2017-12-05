7b0031a0bb73955ce4c7ae66b557c7722459ef55
vannizhang <vannizhang@gmail.com>
2017-12-04 17:14:52 -0800

    remove map.js

--- src/map.js
+++
@@ -1,9 +0,0 @@
-define([], function() {
-    require(["esri/map", "dojo/domReady!"], function(Map) {
-        var map = new Map("mapDiv", {
-            center: [-118, 34.5],
-            zoom: 8,
-            basemap: "topo"
-        });
-    });
-});
